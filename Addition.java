8e5f521764055727f806723112bb45165e8c1554
aarchanaa1234 <128046229+aarchanaa1234@users.noreply.github.com>
2023-03-17 14:34:00 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Addition{
+	
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter A:");
+		
+		a=sc.nextInt();
+				
+		System.out.println("Enter B: ");
+		
+		b=sc.nextInt();
+		
+		c=a+b;
+		
+		System.out.println("Addition :"+c);	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
