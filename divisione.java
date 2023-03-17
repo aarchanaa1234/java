3d5345e19500c9971638f439acac9160b8bbb73a
aarchanaa1234 <128046229+aarchanaa1234@users.noreply.github.com>
2023-03-17 14:06:13 +0530

    Add files via upload

---
+++ divisione.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.util.Scanner;
+
+public class divisione {
+	
+	public static void main(String[] args) {
+		
+		
+		int num1=40,num2=2;
+		
+		int div=num1/num2;
+		
+		System.out.print("divisione of "+num1+" and "+num2+":"+div);
+		
+	}
+		
+
+	}
+
+
